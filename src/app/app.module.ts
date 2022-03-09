c306c6d2b9e024b75cb817040f88e37ef19eee42
MOUHSINE ELACHBI <mouhsineelachbi@gmail.com>
2022-03-09 21:41:58 +0100

    move login component to components folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './components/login/login.component';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
